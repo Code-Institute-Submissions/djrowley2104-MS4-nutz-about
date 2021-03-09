58ae6060fe5be572ba31154c967e0b06978992ee
djrowley2104 <djrowley@hotmail.co.uk>
2021-03-09 12:29:48 +0000

    Data, getting links to work from login to mainsite

--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def mainsite(request):
+    """ link to the mainsite page """
+
+    return render(request, 'home/mainsite.html')    
