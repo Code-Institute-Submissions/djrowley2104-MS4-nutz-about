23175fe3361ff805fd8051df63d829a4fd15336f
djrowley2104 <djrowley@hotmail.co.uk>
2021-04-12 14:55:23 +0000

    Data, getting the hero image to view

--- home/views.py
+++ home/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
 
 
 def index(request):
     """ A view to return the index page """
-
+    if request.user.is_authenticated:
+        return redirect('products')
     return render(request, 'home/index.html')
 
 
